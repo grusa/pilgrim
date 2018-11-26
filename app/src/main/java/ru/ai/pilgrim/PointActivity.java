96cf204d2f768a59dddc923cf98ca407817897b1
grusa <sgrushin70@gmail.com>
2018-11-26 11:26:42 +0300

    261112018 pilgrim

---
+++ app/src/main/java/ru/ai/pilgrim/PointActivity.java
@@ -0,0 +1,13 @@
+package ru.ai.pilgrim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PointActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point);
+    }
+}
---
+++ app/src/main/java/ru/ai/pilgrim/RouteDatabaseHelper.java
@@ -0,0 +1,4 @@
+package ru.ai.pilgrim;
+
+public class RouteDatabaseHelper {
+}
